ec2d535ebc5dc3c9d88de0dfbf1b6bab7199a973
kumudinidesale <kdesale1211@gmail.com>
2022-02-08 20:31:28 +0530

    added:employee is present or absent

---
+++ src/com/emp/EmployeeWage.java
@@ -0,0 +1,20 @@
+package com.emp;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		
+		int full = 1;	
+			double empCheck = Math.floor(Math.random() * 10) % 2;
+				if (empCheck == full)
+					System.out.println("Employee is Present");
+				else
+					System.out.println("Employee is Absent");
+
+			}
+
+		
+
+	}
+
+
